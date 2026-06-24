d129916b3c5beaeb68d800f96cd73cb5a15ca4d9
Corey Williams <Corey.cw01@gmail.com>
2026-06-24 16:55:42 +0000

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    '*.cloudworkstations.dev',
+    '9000-firebase-studio-1782092587116.cluster-rbhjeem4mfgjwrkwwvustjr6em.cloudworkstations.dev',
+    '6000-firebase-studio-1782092587116.cluster-rbhjeem4mfgjwrkwwvustjr6em.cloudworkstations.dev',
   ],
+
   turbopack: {
     root: __dirname,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     remotePatterns: [
       {
